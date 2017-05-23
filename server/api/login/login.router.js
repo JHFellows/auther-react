39a7a9391a9a570774d4310e43ca42a7f068e81d
JHFellows <j.fellows88@gmail.com>
2017-05-23 16:49:35 -0400

    initial commit

---
+++ server/api/login/login.router.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var router = require('express').Router();
+var User = require('../users/user.model')
+
+router.post("/", (req,res,next) => {
+
+})
+
+
+
+
+
+module.exports = router;
